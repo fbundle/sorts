8c00cbddbaea07ee2ff3dc48cc07b2f8e1a89d3f
khanh <khanh.nguyen.contact@gmail.com>
2025-09-05 16:15:12 +0800

    auto_push

--- sorts/v2/inhabited.go
+++ sorts/v2/inhabited.go
@@ -1,6 +1,24 @@
 package sorts
 
+import "github.com/fbundle/sorts/adt"
+
+func newInhabited(ss SortSystem, sort Sort, child Sort) adt.Option[InhabitedSort] {
+	if child.Level() != sort.Level()-1 {
+		return adt.None[InhabitedSort]()
+	}
+	return adt.Some[InhabitedSort](inhabited{
+		Sort:  sort,
+		child: child,
+		ss:    ss,
+	})
+}
+
 type inhabited struct {
-	sort Sort
-	elem Sort
+	Sort
+	child Sort
+	ss    SortSystem
+}
+
+func (i inhabited) Elem() Sort {
+	return i.child
 }
