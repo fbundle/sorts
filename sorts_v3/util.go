6d04a15fb9772402bfb094847f46695919aac0e3
khanh <khanh.nguyen.contact@gmail.com>
2025-09-06 01:24:13 +0800

    auto_push

--- sorts_v3/util.go
+++ sorts_v3/util.go
@@ -1,7 +1,10 @@
 package sorts
 
 func mustType(x Sort, X Sort) {
-	if ok := LessEqual(Parent(x), X); !ok {
+	mustLessEqual(Parent(x), X)
+}
+func mustLessEqual(x Sort, y Sort) {
+	if ok := LessEqual(x, y); !ok {
 		panic("type_error")
 	}
 }
