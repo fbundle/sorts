da2aaf76687224305456fa501709695fd9b9c1ae
khanh <khanh.nguyen.contact@gmail.com>
2025-09-07 03:21:40 +0800

    auto_push

--- sorts_v3/sorts_extra.go
+++ sorts_v3/sorts_extra.go
@@ -17,9 +17,9 @@ type Dependent struct {
 	Apply func(WithSort) WithSort // take x, return B(x)
 }
 
-func (d Dependent) attr() sortAttr {
+func (d Dependent) attr() nameAttr {
 	// Dependent is not a sort so it doesn't have all attributes
-	return sortAttr{
+	return nameAttr{
 		name: d.Name,
 	}
 }
