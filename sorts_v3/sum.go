e30f965e9fc7f6ed015c11f20117588e38a2e972
khanh <khanh.nguyen.contact@gmail.com>
2025-09-08 20:45:07 +0800

    auto_push

--- sorts_v3/sum.go
+++ sorts_v3/sum.go
@@ -40,12 +40,9 @@ func (s Sum) Intro(a Sort, b Sort) Sort {
 // ByCases - take (t: A + B) (h1: A -> X) (h2: B -> X) give (x: X)
 func (s Sum) ByCases(t Sort, h1 Sort, h2 Sort) Sort {
 	mustTermOf(t, s)
-	mustSubType(Parent(h1).(Arrow).A, s.A) // A
-	mustSubType(Parent(h2).(Arrow).A, s.B) // B
-
-	mustSubType(Parent(h1).(Arrow).B, Parent(h2).(Arrow).B) // X
-
 	X := Parent(h1).(Arrow).B
+	mustTermOf(h1, Arrow{s.A, X})
+	mustTermOf(h2, Arrow{s.B, X})
 
 	return dummyTerm(X, fmt.Sprintf("(by_cases %s %s %s)", Name(s), Name(h1), Name(h2)))
 }
