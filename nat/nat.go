83341e3b27dfd3e57d0b38b06de42dedc9a62355
khanh <khanh.nguyen.contact@gmail.com>
2025-09-08 21:39:10 +0800

    auto_push

--- nat/nat.go
+++ nat/nat.go
@@ -10,16 +10,11 @@ var Nat = sorts.NewAtom(0, "Nat", nil)
 
 var Zero = sorts.NewAtom(-1, "0", Nat)
 
-var succSort = sorts.NewAtom(-1, "succ", sorts.Arrow{Nat, Nat})
-
-var Succ = sorts.Inhabited{
-	Sort: succSort,
-	Child: sorts.Arrow{Nat, Nat}.Intro(func(s sorts.Sort) sorts.Sort {
-		sorts.MustTermOf(s, Nat)
-		n, err := strconv.Atoi(sorts.Name(s))
-		if err != nil {
-			panic(err)
-		}
-		return sorts.NewAtom(1, strconv.Itoa(n+1), Nat)
-	}),
-}
+var Succ = sorts.Arrow{Nat, Nat}.Intro(func(s sorts.Sort) sorts.Sort {
+	sorts.MustTermOf(s, Nat)
+	n, err := strconv.Atoi(sorts.Name(s))
+	if err != nil {
+		panic(err)
+	}
+	return sorts.NewAtom(1, strconv.Itoa(n+1), Nat)
+})
