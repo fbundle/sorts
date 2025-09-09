b59539531ef2f506a42a744bf77e6ef5498c3a75
nguyen ngoc khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-09-09 16:23:50 +0800

    auto_push

--- sorts_v4/expr.go
+++ sorts_v4/expr.go
@@ -1,10 +1,34 @@
 package sorts
 
-type Expr interface{}
+type Token string
+
+const (
+	TokenOpen  Token = "("
+	TokenClose Token = ")"
+)
+
+type Expr interface {
+	Marshal() []Token
+}
 
 type Term string
 
+func (e Term) Marshal() []Token {
+	return []Token{Token(e)}
+}
+
 type Node struct {
-	Cmd  Expr
-	Args Expr
+	Cmd  Term
+	Args []Expr
+}
+
+func (e Node) Marshal() []Token {
+	var output []Token
+	output = append(output, TokenOpen)
+	output = append(output, Token(e.Cmd))
+	for _, arg := range e.Args {
+		output = append(output, arg.Marshal()...)
+	}
+	output = append(output, TokenClose)
+	return output
 }
