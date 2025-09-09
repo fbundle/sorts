139584a698edc58c13a6505619777a08db670110
nguyen ngoc khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-09-09 15:08:13 +0800

    auto_push

--- cmd/nat/nat.go
+++ cmd/nat/nat.go
@@ -8,8 +8,11 @@ import (
 )
 
 func main() {
-
 	n0 := nat.Zero
 	n1 := sorts.Arrow{nat.Nat, nat.Nat}.Elim(nat.Succ, n0)
-	fmt.Println(sorts.Name(n0), sorts.Name(n1))
+	n2 := sorts.Arrow{nat.Nat, nat.Nat}.Elim(nat.Succ, n1)
+	n3 := sorts.Arrow{nat.Nat, nat.Nat}.Elim(nat.Succ, n2)
+	for _, n := range []sorts.Sort{n0, n1, n2, n3} {
+		fmt.Println(sorts.Name(n))
+	}
 }
