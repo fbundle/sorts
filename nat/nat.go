0289060bf2dd04ea1e76f2984d11e2f1e2d460a2
nguyen ngoc khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-09-09 15:38:11 +0800

    auto_push

--- nat/nat.go
+++ nat/nat.go
@@ -6,9 +6,13 @@ import (
 	sorts "github.com/fbundle/sorts/sorts_v3"
 )
 
-var Nat = sorts.NewAtom(0, "Nat", nil)
+const (
+	NatLevel = 0
+)
 
-var Zero = sorts.NewAtom(-1, "0", Nat)
+var Nat = sorts.NewAtom(NatLevel+1, "Nat", nil)
+
+var Zero = sorts.NewAtom(NatLevel, "0", Nat)
 
 var NatToNat = sorts.Arrow{A: Nat, B: Nat}
 var NatToNatToNat = sorts.Arrow{A: Nat, B: NatToNat}
@@ -21,9 +25,9 @@ var SuccArrow = NatToNat.Intro("succ", func(a sorts.Sort) sorts.Sort {
 	sorts.MustTermOf(a, Nat)
 	aVal, err := strconv.Atoi(sorts.Name(a))
 	if err != nil {
-		return sorts.NewAtom(-1, err.Error(), Nat)
+		return sorts.NewAtom(NatLevel, err.Error(), Nat)
 	}
-	return sorts.NewAtom(-1, strconv.Itoa(aVal+1), Nat)
+	return sorts.NewAtom(NatLevel, strconv.Itoa(aVal+1), Nat)
 })
 
 func Add(n1 sorts.Sort, n2 sorts.Sort) sorts.Sort {
@@ -35,6 +39,6 @@ var AddArrow = NatToNatToNat.Intro("add", func(a sorts.Sort) sorts.Sort {
 	sorts.MustTermOf(a, Nat)
 	return NatToNat.Intro("add_a", func(b sorts.Sort) sorts.Sort {
 		sorts.MustTermOf(b, Nat)
-		return sorts.NewAtom(-1, "add_a_b", Nat)
+		return sorts.NewAtom(NatLevel, "add_a_b", Nat)
 	})
 })
