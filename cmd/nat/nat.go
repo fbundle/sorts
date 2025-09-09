535202eee2e7700d589e265c2ed6a064ad0e3a89
nguyen ngoc khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-09-09 16:00:11 +0800

    auto_push

--- cmd/nat/nat.go
+++ cmd/nat/nat.go
@@ -3,21 +3,20 @@ package main
 import (
 	"fmt"
 
-	"github.com/fbundle/sorts/nat"
 	sorts "github.com/fbundle/sorts/sorts_v3"
 )
 
 func main() {
-	n0 := nat.Zero
-	n1 := nat.Succ(n0)
-	n2 := nat.Succ(n1)
-	n3 := nat.Succ(n2)
-	n4 := nat.Succ(n3)
-	n5 := nat.Succ(n4)
+	n0 := sorts.Zero
+	n1 := sorts.Succ(n0)
+	n2 := sorts.Succ(n1)
+	n3 := sorts.Succ(n2)
+	n4 := sorts.Succ(n3)
+	n5 := sorts.Succ(n4)
 	for _, n := range []sorts.Sort{n0, n1, n2, n3, n4, n5} {
 		fmt.Println(sorts.Name(n))
 	}
 
-	nx := nat.Add(n2, n3)
+	nx := sorts.Add(n2, n3)
 	fmt.Println(sorts.Name(nx))
 }
---
+++ sorts_v3/nat.go
@@ -0,0 +1,42 @@
+package sorts
+
+import (
+	"strconv"
+)
+
+const (
+	NatLevel = 0
+)
+
+var Nat = NewAtom(NatLevel+1, "Nat", nil)
+
+var Zero = NewAtom(NatLevel, "0", Nat)
+
+var NatToNat = Arrow{A: Nat, B: Nat}
+var NatToNatToNat = Arrow{A: Nat, B: NatToNat}
+
+func Succ(n Sort) Sort {
+	return NatToNat.Elim(SuccArrow, n)
+}
+
+var SuccArrow = NatToNat.Intro("succ", func(a Sort) Sort {
+	MustTermOf(a, Nat)
+	aVal, err := strconv.Atoi(Name(a))
+	if err != nil {
+		return NewAtom(NatLevel, err.Error(), Nat)
+	}
+	return NewAtom(NatLevel, strconv.Itoa(aVal+1), Nat)
+})
+
+func Add(n1 Sort, n2 Sort) Sort {
+	addN1 := NatToNatToNat.Elim(AddArrow, n1)
+	return NatToNat.Elim(addN1, n2)
+}
+
+var AddArrow = NatToNatToNat.Intro("add", func(a Sort) Sort {
+	MustTermOf(a, Nat)
+	return NatToNat.Intro("add_a", func(b Sort) Sort {
+		MustTermOf(b, Nat)
+		return NewAtom(NatLevel, "add_a_b", Nat)
+	})
+})
