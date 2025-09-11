8e9e13d7d411699d1b0454a1b1a3273027ae003c
khanh <khanh.nguyen.contact@gmail.com>
2025-09-11 23:32:37 +0800

    auto_push

--- el/example.el
+++ el/example.el
@@ -7,8 +7,8 @@
     {False : Bool}
     {Nat : U_0}
     {0 : Nat}
-    {succ : Nat -> Nat}
-    {is_two : Nat -> Bool}
+    {succ : {Nat -> Nat}}
+    {is_two : {Nat -> Bool}}
 
     {1 := (succ 0)}
     {2 := (succ 1)}
