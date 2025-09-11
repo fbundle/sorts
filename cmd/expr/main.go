1481ddb2e6415ab4beefcaaf2f7a14570eaea5f8
khanh <khanh.nguyen.contact@gmail.com>
2025-09-11 23:37:05 +0800

    auto_push

--- cmd/expr/main.go
+++ cmd/expr/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "github.com/fbundle/sorts/expr"
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/fbundle/sorts/expr"
+)
 
 func main() {
-	expr.Example()
+	data, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Printf("Error reading stdin: %v\n", err)
+		os.Exit(1)
+	}
+
+	input := string(data)
+
+	tokens := expr.Tokenize(input)
+
+	var e expr.Expr
+	for {
+		e, tokens, err = expr.Parse(tokens)
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Error parsing input: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println(strings.Join(e.Marshal(), " "))
+	}
+
 }
