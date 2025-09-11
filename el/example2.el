283620a5e01d383ff3817419f48122fa023df38a
khanh <khanh.nguyen.contact@gmail.com>
2025-09-12 04:02:09 +0800

    auto_push

---
+++ el/example2.el
@@ -0,0 +1,18 @@
+[
+    {Bool : U_0}
+    {True : Bool}
+    {False : Bool}
+
+    {Nat : U_0}
+    {0 : Nat}
+    {succ : {Nat -> Nat}}
+
+    {1 : Nat}
+    {2 : Nat}
+    {3 : Nat}
+    {1 := (succ 0)}
+    {2 := (succ 1)}
+    {3 := (succ 2)}
+
+    3
+]
