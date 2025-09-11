3885c12401144d7ec41e895feb7d04fedd2ad17a
khanh <khanh.nguyen.contact@gmail.com>
2025-09-12 02:59:26 +0800

    auto_push

--- el/parser_more.go
+++ el/parser_more.go
@@ -15,3 +15,19 @@ var defaultParser = Parser{
 		"match": nil,
 	},
 }
+
+func parseLambda(list []form.Form) (Expr, error) {
+	panic("implement me")
+}
+func parseDefine(list []form.Form) (Expr, error) {
+	panic("implement me")
+}
+func parseAssign(list []form.Form) (Expr, error) {
+	panic("implement me")
+}
+func parseMatch(list []form.Form) (Expr, error) {
+	panic("implement me")
+}
+func parseChain(list []form.Form) (Expr, error) {
+	panic("implement me")
+}
