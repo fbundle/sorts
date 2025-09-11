20e63a2351f0d535ed5b2f42c07369a8c6fa4238
khanh <khanh.nguyen.contact@gmail.com>
2025-09-12 04:52:55 +0800

    auto_push

--- el/example.el
+++ el/example.el
@@ -1,11 +1,11 @@
 [
-    Bool U_0                undefined
-    True Bool               undefined
-    False Bool              undefined
+    Bool U_0                undef
+    True Bool               undef
+    False Bool              undef
 
-    Nat U_0                 undefined
-    0 Nat                   undefined
-    succ {Nat -> Nat}       undefined
+    Nat U_0                 undef
+    0 Nat                   undef
+    succ {Nat -> Nat}       undef
 
     1 Nat (succ 0)
     2 Nat (succ 1)
