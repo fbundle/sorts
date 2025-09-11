9f327b1d7e1444e0868835fd3fe15b05f28eba3b
khanh <khanh.nguyen.contact@gmail.com>
2025-09-11 23:47:17 +0800

    auto_push

--- el/example.el
+++ el/example.el
@@ -5,19 +5,25 @@
     {Bool : U_0}
     {True : Bool}
     {False : Bool}
+
     {Nat : U_0}
     {0 : Nat}
     {succ : {Nat -> Nat}}
-    {is_two : {Nat -> Bool}}
 
+    {1 : Nat}
+    {2 : Nat}
+    {3 : Nat}
     {1 := (succ 0)}
     {2 := (succ 1)}
     {3 := (succ 2)}
+
+    {is_two : {Nat -> Bool}}
     {is_two := {x => (match x
         (succ 1) True
         False
     )}}
 
+    {add: {Nat -> Nat -> Nat}}
     {add := {x => y => (match x
         (succ z) (add z (succ y))   # if x is succ z then add z (succ y)
         y                           # else y
