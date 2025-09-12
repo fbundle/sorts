06d420d00db5c78aaca84148444da3297292c60b
khanh <khanh.nguyen.contact@gmail.com>
2025-09-12 11:12:17 +0800

    auto_push

--- el/runtime.go
+++ el/runtime.go
@@ -55,6 +55,15 @@ func (frame Frame) Get(key Term) (sort sorts.Sort, next Expr, err error) {
 		// Any_0 is at universe level 1
 		// Any_1 is at universe level 2
 		return sorts.NewAtom(level+1, sorts.TerminalName, nil), key, nil
+	} else if strings.HasPrefix(keyStr, "Unit_") {
+		levelStr := strings.TrimPrefix(keyStr, "Unit_")
+		level, err := strconv.Atoi(levelStr)
+		if err != nil {
+			return nil, nil, notFoundErr
+		}
+		// Unit_0 is at universe level 1
+		// Unit_1 is at universe level 2
+		return sorts.NewAtom(level+1, sorts.InitialName, nil), key, nil
 	} else {
 		return nil, nil, notFoundErr
 	}
