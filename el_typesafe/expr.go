1424eae6604973bb2d165aadf6638202fa8c5bcf
khanh <khanh.nguyen.contact@gmail.com>
2025-09-13 00:31:11 +0800

    auto_push

---
+++ el_typesafe/expr.go
@@ -0,0 +1,38 @@
+package el_typesafe
+
+type Expr interface {
+	mustExpr()
+}
+
+// partialExpr - those Expr that type cannot be totally resolved
+// need type binding - like lambda
+type partialExpr interface {
+	Expr
+	resolvePartial(frame Frame) (Frame, partialObject)
+}
+
+// totalExpr - those Expr that type can be totally resolved
+// no need type binding - like term
+type totalExpr interface {
+	Expr
+	resolveTotal(frame Frame) (Frame, totalObject)
+}
+
+type Term string
+
+func (t Term) mustExpr() {}
+func (t Term) resolveTotal(frame Frame) (Frame, totalObject) {
+	panic("not implemented")
+}
+
+func resolvePartial(frame Frame, expr Expr) (Frame, partialObject) {
+	switch expr := expr.(type) {
+	case partialExpr:
+		return expr.resolvePartial(frame)
+	case totalExpr:
+		frame, o := expr.resolveTotal(frame)
+		return frame, o.partial()
+	default:
+		panic("type_error")
+	}
+}
---
+++ el_typesafe/object.go
@@ -0,0 +1,56 @@
+package el_typesafe
+
+import (
+	"github.com/fbundle/sorts/persistent/ordered_map"
+	"github.com/fbundle/sorts/sorts"
+)
+
+type object struct {
+	sort sorts.Sort
+	next Expr
+}
+
+type totalObject struct {
+	object
+}
+
+func newTotalObject(sort sorts.Sort, next Expr) totalObject {
+	if sort == nil || next == nil {
+		panic("type_error")
+	}
+	return totalObject{object{sort: sort, next: next}}
+}
+
+func (o totalObject) parent() totalObject {
+	panic("not implemented")
+}
+func (o totalObject) partial() partialObject {
+	return partialObject{object: o.object}
+}
+
+type partialObject struct {
+	object
+}
+
+func newPartialObject(next Expr) partialObject {
+	if next == nil {
+		panic("type_error")
+	}
+	return partialObject{object{sort: nil, next: next}}
+}
+
+func (o partialObject) typeCheck(frame Frame, parentSort sorts.Sort) (totalObject, bool) {
+	// convert a partial object to a total object using type-check
+	panic("not implemented")
+}
+
+type Frame struct {
+	dict ordered_map.OrderedMap[Term, totalObject]
+}
+
+func (frame Frame) set(key Term, o totalObject) {
+	panic("not implemented")
+}
+func (frame Frame) get(key Term) (totalObject, bool) {
+	panic("not implemented")
+}
