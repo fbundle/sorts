e042228f8facb146b962a32fee2bda20aa75eb33
khanh <khanh.nguyen.contact@gmail.com>
2025-09-12 18:47:13 +0800

    auto_push

--- el/match.go
+++ el/match.go
@@ -65,7 +65,6 @@ func reverseMatchPattern(frame Frame, condSort sorts.Sort, pattern Expr) (Frame,
 			return frame, errors.New("expected function")
 		}
 		if !sorts.SubTypeOf(cmdArrow.B, sorts.Parent(condSort)) {
-
 			return frame, errors.New("wrong output type")
 		}
 		return reverseMatchPattern(frame, sorts.NewTerm(cmdArrow.A, String(pattern.Arg)), pattern.Arg)
