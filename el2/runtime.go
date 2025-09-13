fe652b135783e16f81c6e296559bb66d3270ff20
khanh <khanh.nguyen.contact@gmail.com>
2025-09-13 23:27:07 +0800

    auto_push

--- el2/runtime.go
+++ el2/runtime.go
@@ -1,7 +1,6 @@
 package el2
 
 import (
-	"cmp"
 	"strconv"
 	"strings"
 
@@ -17,18 +16,6 @@ func DefaultRuntime() Runtime {
 		NewListParser("->", sorts.ListParseArrow("->")).
 		NewListParser("⊕", sorts.ListParseSum("⊕")).
 		NewListParser("⊗", sorts.ListParseProd("⊗"))
-}
-
-type rule struct {
-	src sorts.Name
-	dst sorts.Name
-}
-
-func (r rule) Cmp(s rule) int {
-	if c := cmp.Compare(r.src, s.src); c != 0 {
-		return c
-	}
-	return cmp.Compare(r.dst, s.dst)
 }
 
 type Runtime struct {
---
+++ el2/util.go
@@ -0,0 +1,19 @@
+package el2
+
+import (
+	"cmp"
+
+	"github.com/fbundle/sorts/sorts"
+)
+
+type rule struct {
+	src sorts.Name
+	dst sorts.Name
+}
+
+func (r rule) Cmp(s rule) int {
+	if c := cmp.Compare(r.src, s.src); c != 0 {
+		return c
+	}
+	return cmp.Compare(r.dst, s.dst)
+}
