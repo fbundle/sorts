fd00e1cefcc1f9a1da09f15e4aa79aad5169faa5
khanh <khanh.nguyen.contact@gmail.com>
2025-09-14 02:56:38 +0800

    auto_push

--- el2/object.go
+++ el2/object.go
@@ -2,23 +2,32 @@ package el2
 
 // AlmostSort - almost a sort - for example, a lambda
 type AlmostSort interface {
+	Sort() Sort // nullable
 	TypeCheck(sa SortAttr, parent Sort) Sort
 }
 
 // Object - a sort
 type Object struct {
-	Sort
+	sort Sort
+}
+
+func (s Object) Sort() Sort {
+	return s.sort
 }
 
 func (s Object) TypeCheck(sa SortAttr, parent Sort) Sort {
-	must(sa).termOf(s, parent)
-	return s.Sort
+	must(sa).termOf(s.sort, parent)
+	return s.sort
 }
 
 // Beta - beta reduction
 type Beta struct {
 	Cmd Form
 	Arg Form
+}
+
+func (f Beta) Sort() Sort {
+	return nil
 }
 
 func (f Beta) TypeCheck(sa SortAttr, parent Sort) Sort {
@@ -30,6 +39,10 @@ func (f Beta) TypeCheck(sa SortAttr, parent Sort) Sort {
 type Lambda struct {
 	Param Name
 	Body  Form
+}
+
+func (l Lambda) Sort() Sort {
+	return nil
 }
 
 func (l Lambda) TypeCheck(sa SortAttr, parent Sort) Sort {
