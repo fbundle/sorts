d1d542bf9bda089cba8078341090bbfef4a624a2
khanh <khanh.nguyen.contact@gmail.com>
2025-09-13 23:41:55 +0800

    auto_push

--- el2/object.go
+++ el2/object.go
@@ -9,6 +9,7 @@ type Name = form.Name
 type Form = form.Form
 type List = form.List
 type Sort = sorts.Sort
+type SortAttr = sorts.SortAttr
 
 var NewAtomChain = sorts.NewAtomChain
 var NewAtomTerm = sorts.NewAtomTerm
@@ -21,6 +22,14 @@ var ListParseProd = sorts.ListParseProd
 
 // AlmostSort - almost a sort - for example, a lambda
 type AlmostSort interface {
+	TypeCheck(sa SortAttr, parent Sort) Sort
+}
+
+type actualSort struct {
 	Sort
-	TypeCheck(parent Sort) Sort
+}
+
+func (s actualSort) TypeCheck(sa SortAttr, parent Sort) Sort {
+	must(sa).termOf(s, parent)
+	return s.Sort
 }
--- el2/util.go
+++ el2/util.go
@@ -2,6 +2,7 @@ package el2
 
 import (
 	"cmp"
+	"fmt"
 
 	"github.com/fbundle/sorts/sorts"
 )
@@ -16,4 +17,26 @@ func (r rule) Cmp(s rule) int {
 		return c
 	}
 	return cmp.Compare(r.dst, s.dst)
+}
+
+func must(a SortAttr) mustSortAttr {
+	return mustSortAttr{a}
+}
+
+var TypeErr = fmt.Errorf("type_error")
+
+type mustSortAttr struct {
+	a SortAttr
+}
+
+func (m mustSortAttr) lessEqual(x Sort, y Sort) {
+	if !m.a.LessEqual(x, y) {
+		panic(TypeErr)
+	}
+}
+
+func (m mustSortAttr) termOf(x Sort, X Sort) {
+	if !m.a.LessEqual(m.a.Parent(x), X) {
+		panic(TypeErr)
+	}
 }
