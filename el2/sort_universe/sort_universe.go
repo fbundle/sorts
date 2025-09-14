ed908521a39039a59a39d5440f029f4dc16c43b0
khanh <khanh.nguyen.contact@gmail.com>
2025-09-14 17:36:43 +0800

    auto_push

--- el2/sort_universe/sort_universe.go
+++ el2/sort_universe/sort_universe.go
@@ -2,34 +2,36 @@ package sort_universe
 
 import (
 	"cmp"
+	"errors"
 	"strconv"
 	"strings"
 
-	"github.com/fbundle/sorts/el2"
 	"github.com/fbundle/sorts/form"
 	"github.com/fbundle/sorts/persistent/ordered_map"
 	"github.com/fbundle/sorts/sorts"
 )
 
+var TypeErr = errors.New("type_error")
+
 type SortUniverse struct {
-	initialHeader  form.Name
-	terminalHeader form.Name
-	nameLessEqual  ordered_map.Map[rule] // use Map since rule is not of cmp.Ordered
+	InitialTypeName  form.Name
+	TerminalTypeName form.Name
+	nameLessEqual    ordered_map.Map[rule] // use Map since rule is not of cmp.Ordered
 
 }
 
 func (u SortUniverse) mustSortAttr() SortUniverse {
-	if u.initialHeader == u.terminalHeader {
-		panic(el2.TypeErr)
+	if u.InitialTypeName == u.TerminalTypeName {
+		panic(TypeErr)
 	}
 	return u
 }
 
-func (u SortUniverse) parseConstant(key form.Name) (el2.Sort, bool) {
+func (u SortUniverse) parseConstant(key form.Name) (sorts.Sort, bool) {
 	// parse builtin: initial, terminal
-	builtin := map[form.Name]func(level int) el2.Sort{
-		u.initialHeader:  u.Initial,
-		u.terminalHeader: u.Terminal,
+	builtin := map[form.Name]func(level int) sorts.Sort{
+		u.InitialTypeName:  u.Initial,
+		u.TerminalTypeName: u.Terminal,
 	}
 	name := string(key)
 	for header, makeFunc := range builtin {
@@ -51,23 +53,23 @@ func (u SortUniverse) NewNameLessEqualRule(src form.Name, dst form.Name) SortUni
 	return u
 }
 
-func (u SortUniverse) newTerm(name form.Name, parent el2.Sort) el2.Sort {
-	return el2.NewAtomTerm(u, name, parent)
+func (u SortUniverse) newTerm(name form.Name, parent sorts.Sort) sorts.Sort {
+	return sorts.NewAtomTerm(u, name, parent)
 }
 
 // Terminal - T_0 T_1 ... T_n
-func (u SortUniverse) Terminal(level int) el2.Sort {
-	return el2.NewAtomChain(level, func(level int) form.Name {
+func (u SortUniverse) Terminal(level int) sorts.Sort {
+	return sorts.NewAtomChain(level, func(level int) form.Name {
 		levelStr := form.Name(strconv.Itoa(level))
-		return u.terminalHeader + "_" + levelStr
+		return u.TerminalTypeName + "_" + levelStr
 	})
 }
 
 // Initial - I_0 I_1 ... I_n
-func (u SortUniverse) Initial(level int) el2.Sort {
-	return el2.NewAtomChain(level, func(level int) form.Name {
+func (u SortUniverse) Initial(level int) sorts.Sort {
+	return sorts.NewAtomChain(level, func(level int) form.Name {
 		levelStr := form.Name(strconv.Itoa(level))
-		return u.initialHeader + "_" + levelStr
+		return u.InitialTypeName + "_" + levelStr
 	})
 }
 
@@ -89,7 +91,7 @@ func (u SortUniverse) TermOf(x sorts.Sort, X sorts.Sort) bool {
 }
 
 func (u SortUniverse) NameLessEqual(src sorts.Name, dst sorts.Name) bool {
-	if src == u.initialHeader || dst == u.terminalHeader {
+	if src == u.InitialTypeName || dst == u.TerminalTypeName {
 		return true
 	}
 	if src == dst {
