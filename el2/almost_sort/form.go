95ac285f877b511b0a96a0b11ee6f14906c9304c
khanh <khanh.nguyen.contact@gmail.com>
2025-09-14 17:50:20 +0800

    auto_push

--- el2/almost_sort/form.go
+++ el2/almost_sort/form.go
@@ -1,35 +1,9 @@
 package almost_sort
 
 import (
-	"fmt"
-
 	"github.com/fbundle/sorts/form"
 	"github.com/fbundle/sorts/sorts"
 )
-
-type ParseFunc = func(form form.Form) AlmostSort
-type ListParseFunc = func(parse ParseFunc, list form.List) AlmostSort
-
-var TypeErr = fmt.Errorf("type_error")
-
-func MustSort(as AlmostSort) sorts.Sort {
-	return as.(ActualSort).Sort
-}
-
-// AlmostSort - almost a Sort - for example, a lambda
-type AlmostSort interface {
-	TypeCheck(sa sorts.SortAttr, parent sorts.Sort) sorts.Sort // not nullable
-}
-
-// ActualSort - a Sort
-type ActualSort struct {
-	Sort sorts.Sort
-}
-
-func (s ActualSort) TypeCheck(sa sorts.SortAttr, parent sorts.Sort) sorts.Sort {
-	must(sa).termOf(s.Sort, parent)
-	return s.Sort
-}
 
 func ListParseBeta(parse ParseFunc, list form.List) AlmostSort {
 	if len(list) != 2 {
