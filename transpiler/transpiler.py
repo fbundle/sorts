0e3ea4b56ec71a3fbbd06b64ed03f785fe26bb64
nguyen ngoc khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-09-15 12:58:01 +0800

    auto_push

--- transpiler/transpiler.py
+++ transpiler/transpiler.py
@@ -1,8 +1,30 @@
+class IndentedLine(pydantic.BaseModel):
+    indentation: int
+    line: list[str]
 
+def main(source: str):
+    lines: list[str] = source.split("\n")
+
+    indented_lines: list[IndentedLine] = []
+    for line in lines:
+        indentation: int = 0
+        for ch in line:
+            if ch == " ":
+                indentation += 1
+            elif ch == "\t":
+                indentation += 2
+            else:
+                break
+        indented_lines.append(Line(
+            indentation=indentation,
+            line=line.split(),
+        ))
+    
+    print(indented_lines)
 
 
 
 
 
 if __name__ == "__main__":
-    ...
+    main(open(sys.argv[0]).read())
