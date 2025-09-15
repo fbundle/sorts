d7ca66c154719a15109d654a26ad15f453e050d6
khanh <khanh.nguyen.contact@gmail.com>
2025-09-15 17:58:00 +0800

    auto_push

--- sorts/dept.go
+++ sorts/dept.go
@@ -1,5 +1,13 @@
 package sorts
 
+/*
+// Inductive - inductive type
+type Inductive interface {
+	Sort
+	Iter(yield func(name form.Name, constr func([]Sort) Inductive) bool)
+}
+*/
+
 // Dept - represent a type B(x) depends on Sort x
 type Dept struct {
 	Form   Form
@@ -18,16 +26,7 @@ must_pos_type = lambda (x: Nat)
 	 | n0        => Nil
 
 in this example must_pos is of type Π_{x: Nat} must_pos_type(x)
-from current type checking, we know that this is a subtype of Nat -> (Nat + Nil)
-
-*/
-
-/*
-// Inductive - inductive type
-type Inductive interface {
-	Sort
-	Iter(yield func(name form.Name, constr func([]Sort) Inductive) bool)
-}
+from current type checking, we know that this is a subtype of Nat -> (Nat ⊕ Nil)
 */
 
 // Pi - dependent function type Π_{x: A} B(x)
@@ -38,6 +37,33 @@ type Pi struct {
 }
 
 func (s Pi) sortAttr(a SortAttr) sortAttr {
+	return sortAttr{
+		form: List{s.H, a.Form(s.A), a.Form(s.B)},
+	}
+}
+
+/*
+must_pos = lambda (x: Nat)
+  match x with
+	 | succ z    => (x ⊕ x)
+	 | n0        => (x ⊕ nil)
+
+must_pos_type = lambda (x: Nat)
+	match x with
+	 | succ z    => (Nat ⊕ Nat)
+	 | n0        => (Nat ⊕ Nil)
+
+in this example, must_pos is of type Σ_{x: Nat} must_pos_type(x)
+*/
+
+// Sigma - dependent pair type Σ_{x: A} B(x)
+type Sigma struct {
+	H Name
+	A Sort
+	B Dept
+}
+
+func (s Sigma) sortAttr(a SortAttr) sortAttr {
 	return sortAttr{
 		form: List{s.H, a.Form(s.A), a.Form(s.B)},
 	}
