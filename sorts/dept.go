5b2a56dfb684841f14714ddc360a6bef8552fbf4
khanh <khanh.nguyen.contact@gmail.com>
2025-09-15 17:51:29 +0800

    auto_push

---
+++ sorts/dept.go
@@ -0,0 +1,28 @@
+package sorts
+
+// Dept - represent a type B(x) depends on Sort x
+type Dept struct {
+	Form   Form
+	Lambda Lambda
+}
+
+/*
+// Inductive - inductive type
+type Inductive interface {
+	Sort
+	Iter(yield func(name form.Name, constr func([]Sort) Inductive) bool)
+}
+*/
+
+// Pi - dependent function type Π_{x: A} B(x)
+type Pi struct {
+	H Name
+	A Sort
+	B Dept
+}
+
+func (s Pi) sortAttr(a SortAttr) sortAttr {
+	return sortAttr{
+		form: List{s.H, a.Form(x), a.Form(s.B)},
+	}
+}
