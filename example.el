d9161e621e1ae15c95912f23c0a69eaf6c4eb7c7
khanh <khanh.nguyen.contact@gmail.com>
2025-09-15 20:24:51 +0800

    auto_push

--- example.el
+++ example.el
@@ -21,14 +21,14 @@
         {n0       => False}
     )}}
 
-    (:= must_pos (=> (: x Nat) (match x
-        (=> (succ z)   x)
-        (=> n0         nil)
-    )))
+    {must_pos := {{x : Nat} => (match x
+        {(succ z) => x}
+        {n0       => nil}
+    )}}
 
 
-    (:= _ (inspect is_pos))                    # resolved type as       Nat -> Bool
-    (:= _ (inspect must_pos))                  # resolved type as       Nat -> (Nat ⊕ Nil)
+    {_ := (inspect is_pos)}                    # resolved type as       Nat -> Bool
+    {_ := (inspect must_pos)}                  # resolved type as       Nat -> (Nat ⊕ Nil)
                                                # better to resolve as   Π_{x: Nat} B(x) where B(x) = (type (must_pos x))
 
     Unit_0
