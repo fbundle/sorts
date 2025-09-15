d09f039d2139264d893232381ef18402e600448c
nguyen ngoc khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-09-15 13:09:18 +0800

    auto_push

--- example.el
+++ example.el
@@ -1,34 +1,37 @@
 (let
-    {Nil := (inh Any_2)}
-    {nil := (inh Nil)}
-    {Bool := (inh Any_2)}
-    {True := (inh Bool)}
-    {False := (inh Bool)}
+    (:= Nil (inh Any_2))
+    (:= nil (inh Nil))
+    (:= Bool (inh Any_2))
+    (:= True (inh Bool))
+    (:= False (inh Bool))
 
-    {Nat := (inh Any_2)}
-    {n0 := (inh Nat)}
-    {succ := (inh {Nat -> Nat})}
+    (:= Nat (inh Any_2))
+    (:= n0 (inh Nat))
+    (:= succ (inh (-> Nat Nat)))
 
-    {n1 := (succ n0)}
-    {n2 := (succ n1)}
-    {n3 := (succ n2)}
-    {n4 := (succ n3)}
+    (:= n1 (succ n0))
+    (:= n2 (succ n1))
+    (:= n3 (succ n2))
+    (:= n4 (succ n3))
 
-    {x := {n1 ⊕ n2 ⊕ n3}}
-    {x := {n1 ⊗ n2 ⊗ n3 ⊗ n4}}
+    (:= x {n1 ⊕ n2 ⊕ n3})
+    (:= x {n1 ⊗ n2 ⊗ n3 ⊗ n4})
 
-    {is_pos := {{x: Nat} => (match x
-        {(succ z)   => True}
-        {n0         => False}
-    )}}
+    (:= is_pos (=> (: x Nat) (match x
+        (=> (succ z)   True)
+        (=> n0         False)
+    )))
 
-    {must_pos := {{x: Nat} => (match x
-        {(succ z)   => x}
-        {n0         => nil}
-    )}}
+    (:= must_pos (=> (: x Nat) (match x
+        (=> (succ z)   x)
+        (=> n0         nil)
+    )))
 
-    # is_pos                  # resolved type as Nat -> Bool
-    must_pos                  # resolved type as Nat -> (Nat + Nil)
+
+    (:= _ (inspect is_pos))                  # resolved type as Nat -> Bool
+    (:= _ (inspect must_pos))                  # resolved type as Nat -> (Nat + Nil)
+
+    Unit_0
 )
 
 
