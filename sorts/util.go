20437e29f6f905d808d88f556992180aa5924151
khanh <khanh.nguyen.contact@gmail.com>
2025-09-17 16:33:05 +0800

    auto_push

--- sorts/util.go
+++ sorts/util.go
@@ -1,5 +1,7 @@
 package sorts
 
+import "fmt"
+
 func serialize(s Sort) []Sort {
 	if s, ok := s.(Arrow); ok {
 		body := serialize(s.B)
@@ -7,4 +9,15 @@ func serialize(s Sort) []Sort {
 	} else {
 		return []Sort{s}
 	}
+}
+
+func deserialize(s []Sort) Sort {
+	if len(s) == 0 {
+		panic(fmt.Errorf("empty sort array"))
+	}
+	output := s[len(s)-1]
+	for i := len(s) - 2; i >= 0; i-- {
+		output = Arrow{s[i], output}
+	}
+	return output
 }
