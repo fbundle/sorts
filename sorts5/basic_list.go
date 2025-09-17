ae3dda0a4a09b42aef9ab6de4a97b661b800b260
khanh <khanh.nguyen.contact@gmail.com>
2025-09-17 15:21:48 +0800

    auto_push

--- sorts5/basic_list.go
+++ sorts5/basic_list.go
@@ -9,23 +9,32 @@ type Arrow struct {
 	B Sort
 }
 
+func (s Arrow) Compile(ctx Context) Sort {
+	s.A = s.A.Compile(ctx)
+	s.B = s.B.Compile(ctx)
+	return s
+}
+
 func (s Arrow) Form() Form {
 	return List{ArrowCmd, s.A.Form(), s.B.Form()}
 }
 
-func (s Arrow) Level() int {
-	//TODO implement me
-	panic("implement me")
+func (s Arrow) Level(ctx Context) int {
+	return max(s.A.Level(ctx), s.B.Level(ctx))
 }
 
-func (s Arrow) Parent() Sort {
-	//TODO implement me
-	panic("implement me")
+func (s Arrow) Parent(ctx Context) Sort {
+	return Arrow{
+		A: s.A.Parent(ctx),
+		B: s.B.Parent(ctx),
+	}
 }
 
-func (s Arrow) LessEqual(dst Sort) bool {
-	//TODO implement me
-	panic("implement me")
+func (s Arrow) LessEqual(ctx Context, d Sort) bool {
+	if d, ok := d.(Arrow); ok {
+		return d.A.LessEqual(ctx, s.A) && s.B.LessEqual(ctx, d.B)
+	}
+	return ctx.LessEqual(s.Form(), d.Form())
 }
 
 var _ Sort = Arrow{}
