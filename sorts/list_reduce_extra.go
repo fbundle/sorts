16ae470096fded1dcf3d5ff5ed77665a6f6091a6
khanh <khanh.nguyen.contact@gmail.com>
2025-09-17 17:24:51 +0800

    auto_push

--- sorts/list_reduce_extra.go
+++ sorts/list_reduce_extra.go
@@ -6,7 +6,15 @@ const (
 
 func init() {
 	ListParseFuncMap[AnnotCmd] = func(ctx Context, list List) Sort {
+		err := parseErr(AnnotCmd, []string{"name", "type"})
+		if len(list) != 2 {
+			panic(err)
+		}
 
+		return Annot{
+			Name: mustName(err, list[0]),
+			Type: ctx.Parse(list[1]),
+		}
 	}
 }
 
@@ -15,8 +23,34 @@ type Annot struct {
 	Type Sort
 }
 
-func (a Annot) Form() Form {
-	return List{AnnotCmd, a.Name, a.Type.Form()}
+func (s Annot) Form() Form {
+	return List{AnnotCmd, s.Name, s.Type.Form()}
 }
 
-var _ Code = Annot{}
+func (s Annot) Compile(ctx Context) Sort {
+	return Annot{
+		Name: s.Name,
+		Type: s.Type.Compile(ctx),
+	}
+}
+
+func (s Annot) Level(ctx Context) int {
+	return s.Type.Level(ctx)
+}
+
+func (s Annot) Parent(ctx Context) Sort {
+	return s.Type.Parent(ctx)
+}
+
+func (s Annot) LessEqual(ctx Context, d Sort) bool {
+	return s.Type.LessEqual(ctx, d)
+}
+
+func (s Annot) Reduce(ctx Context) Sort {
+	return Annot{
+		Name: s.Name,
+		Type: s.Type.Reduce(ctx),
+	}
+}
+
+var _ Sort = Annot{}
