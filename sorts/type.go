8b5e1512e22fd4b63ad5c4863dee8f83de23a63a
khanh <khanh.nguyen.contact@gmail.com>
2025-09-24 15:38:21 +0800

    auto_push

--- sorts/type.go
+++ sorts/type.go
@@ -15,3 +15,14 @@ func (s Type) Form() Form {
 func (s Type) Parent(ctx Context) Sort {
 	return s.Body.Parent(ctx).Parent(ctx)
 }
+
+func (s Type) Level(ctx Context) int {
+	return s.Body.Level(ctx) + 1
+}
+func (s Type) LessEqual(ctx Context, d Sort) bool {
+	return s.Body.Parent(ctx).LessEqual(ctx, d)
+}
+
+func (s Type) Reduce(ctx Context) Sort {
+	return s.Body.Parent(ctx)
+}
