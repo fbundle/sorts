fdafc673f81505011455ef15918fedbf348c337f
khanh <khanh.nguyen.contact@gmail.com>
2025-09-25 00:42:11 +0800

    auto_push

--- sorts/sorts.go
+++ sorts/sorts.go
@@ -1,16 +1,12 @@
 package sorts
 
 import (
-	"errors"
-
 	"github.com/fbundle/sorts/form"
 )
 
 type Name = form.Name
 type List = form.List
 type Form = form.Form
-
-var TypeErr = errors.New("type_error")
 
 type Sort interface {
 	Form() Form
