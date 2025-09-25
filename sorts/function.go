428f92a15fc7789a564f96f54b8ef8973a4da679
khanh <khanh.nguyen.contact@gmail.com>
2025-09-25 09:10:11 +0800

    auto_push

--- sorts/function.go
+++ sorts/function.go
@@ -1,6 +1,42 @@
 package sorts
 
 func init() {
+	DefaultParseFunc = func(ctx Context, list List) Sort {
+		err := compileErr(list, []string{"cmd", "arg"})
+		if len(list) != 2 {
+			panic(err)
+		}
+		return Beta{
+			Cmd: mustType[Lambda](err, ctx.Parse(list[0])),
+			Arg: ctx.Parse(list[1]),
+		}
+	}
+}
+
+type Beta struct {
+	Cmd Lambda
+	Arg Sort
+}
+
+func (s Beta) Form() Form {
+	return List{s.Cmd.Form(), s.Arg.Form()}
+}
+
+func (s Beta) Parent(ctx Context) Sort {
+	return s.Cmd.Body.Parent(ctx.Set(s.Cmd.Param.Name, s.Arg))
+}
+func (s Beta) Level(ctx Context) int {
+	panic("not_implemented")
+}
+
+func (s Beta) LessEqual(ctx Context, d Sort) bool {
+	panic("not_implemented")
+}
+func (s Beta) Eval(ctx Context) Sort {
+	return s.Cmd.Body.Eval(ctx.Set(s.Cmd.Param.Name, s.Arg))
+}
+
+func init() {
 	ListParseFuncMap[LambdaCmd] = func(ctx Context, list List) Sort {
 		err := compileErr(list, []string{string(LambdaCmd), "param", "body"})
 		if len(list) != 2 {
