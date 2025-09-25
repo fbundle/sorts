676864ee0c94cf97cab819bc1f8e8f069cf1d9b1
khanh <khanh.nguyen.contact@gmail.com>
2025-09-25 08:40:47 +0800

    auto_push

--- sorts/other.go
+++ sorts/other.go
@@ -23,3 +23,27 @@ type Annot struct {
 func (a Annot) Form() Form {
 	return List{AnnotCmd, a.Name, a.Type.Form()}
 }
+
+func compileBinding(ctx Context, list List) Binding {
+	err := compileErr(list, []string{string(BindingCmd), "name", "value"})
+	if len(list) != 2 {
+		panic(err)
+	}
+	return Binding{
+		Name:  mustType[Name](err, list[0]),
+		Value: ctx.Parse(list[1]),
+	}
+}
+
+const (
+	BindingCmd Name = ":="
+)
+
+type Binding struct {
+	Name  Name
+	Value Sort
+}
+
+func (b Binding) Form() Form {
+	return List{BindingCmd, b.Name, b.Value.Form()}
+}
