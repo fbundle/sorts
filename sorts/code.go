acf027c530d8f6adf84ba6fea5c22fee42aa963d
khanh <khanh.nguyen.contact@gmail.com>
2025-09-25 16:12:25 +0800

    auto_push

--- sorts/code.go
+++ sorts/code.go
@@ -73,7 +73,16 @@ func (c Beta) Form() Form {
 }
 
 func (c Beta) Eval(ctx Context) Sort {
-	cmd := mustType[Pi](TypeError, c.Cmd.Eval(ctx))
+	cmdSort := c.Cmd.Eval(ctx)
+	var cmd Pi
+	switch t := cmdSort.(type) {
+	case Pi:
+		cmd = t
+	case piWithName:
+		cmd = t.Pi
+	default:
+		panic(TypeError)
+	}
 	if len(cmd.Params) != len(c.Args) {
 		panic(TypeError)
 	}
