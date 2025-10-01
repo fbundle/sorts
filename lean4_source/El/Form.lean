1176b37461025420210010edb2bf36321b216c13
khanh <khanh.nguyen.contact@gmail.com>
2025-10-01 11:22:09 +0800

    auto_push

---
+++ lean4_source/El/Form.lean
@@ -0,0 +1,26 @@
+namespace Form
+
+universe u
+
+inductive Form where
+  | name: String → Form
+  | list: List Form → Form
+  deriving Repr
+
+#eval (Form.name "hello" : Form)
+#eval (Form.list [Form.name "hello", Form.name "world"] : Form)
+
+
+def sortSplitTokens (splitTokens : List String) : List String :=
+  -- sort tokens so that if s2 is a prefix of s1, s1 should come first
+  sorry
+
+def tokenize (sortedSplitTokens : List String) (s : String) : String :=
+
+
+
+  sorry
+
+
+
+end Form
