0353a789a7a7be95fed5be7277828ac0e482a9e7
Nguyen Ngoc Khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-10-14 15:12:38 +0800

    auto_push

--- lean4_source/El/Code.lean
+++ lean4_source/El/Code.lean
@@ -1,10 +1,25 @@
-structure Atom where
-  name: String
+structure Beta (α: Type) where
+  cmd: α
+  args: List α
 
 structure Annot (α: Type) where
   name: String
   type: α
 
+structure Binding (α: Type) where
+  name: String
+  value: α
+
+structure Type (α: Type) where
+  value: α
+
+structure Inhabited (α: Type) where
+  value: α
+
 structure Pi (α: Type) where
   params: List (Annot α)
   body: α
+
+structure Inductive (α: Type) where
+  type: Annot α
+  cons: List (Annot α)
