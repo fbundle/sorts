34cec0e177ca490bd2685140be0db4de3291639a
Nguyen Ngoc Khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-10-14 16:55:25 +0800

    auto_push

--- lean4_source/El/Code.lean
+++ lean4_source/El/Code.lean
@@ -138,9 +138,9 @@ end
 
 
 def _example := "
-  (:= Nat (inh U_2))
-  (:= 0 (inh Nat))
-  (:= succ (inh (-> Nat)))
+  (:= Nat (* U_2))
+  (:= 0 (* Nat))
+  (:= succ (* (-> Nat)))
 
   (:= 1 (succ 0))
   (:= 2 (succ 0))
