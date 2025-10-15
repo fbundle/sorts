89d0918245acf98d4e6012fe3f980162faf04593
khanh <khanh.nguyen.contact@gmail.com>
2025-10-15 22:49:39 +0800

    auto_push

--- EL/EL.lean
+++ EL/EL.lean
@@ -10,36 +10,4 @@ def parse (tokens: List String): Option (List String × (Code Atom)) := do
   let code ← parseCode parseAtom form
   pure (tokens, code)
 
-
-private def source := "
-  (let Nat (*U_2))
-  (let n0 (*Nat))
-  (let succ (*(-> Nat)))
-
-  (let n1 (succ n0))
-  (let n2 (succ n0))
-
-  (let x 3)
-  (let y 4)
-
-  (+ x y)
-
-  (ind (: Nat U_2)
-    (: zero (=> Nat))
-    (: succ (=> (: _ Nat) Nat))
-  )
-
-  (let one (succ zero))
-  (let two (succ one))
-
-  (let is_pos
-    (=> (: n Nat) (match n
-      (-> (Nat.zero) false)
-      (-> (Nat.succ x) true)
-    ))
-  )
-"
-
-#eval Util.parseAll parse (tokenize source)
-
 end EL
--- Main.lean
+++ Main.lean
@@ -1,4 +1,37 @@
+import EL.EL
 
+def source := "
+  (let Nat (*U_2))
+  (let n0 (*Nat))
+  (let succ (*(-> Nat)))
+
+  (let n1 (succ n0))
+  (let n2 (succ n0))
+
+  (let x 3)
+  (let y 4)
+
+  (+ x y)
+
+  (ind (: Nat U_2)
+    (: zero (=> Nat))
+    (: succ (=> (: _ Nat) Nat))
+  )
+
+  (let one (succ zero))
+  (let two (succ one))
+
+  (let is_pos
+    (=> (: n Nat) (match n
+      (-> (Nat.zero) false)
+      (-> (Nat.succ x) true)
+    ))
+  )
+"
+
+#eval Util.parseAll EL.parse (EL.tokenize source)
 
 def main : IO Unit :=
-  IO.println s!"Hello, World!"
+  let x := Util.parseAll EL.parse (EL.tokenize source)
+
+  IO.println s!"{repr x}"
