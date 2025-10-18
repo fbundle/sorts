9eaa9a2bc731da5f86f7f265d3c5dd7626e51eb7
khanh <khanh.nguyen.contact@gmail.com>
2025-10-19 00:37:35 +0800

    auto_push

--- EL2/Kernel.lean
+++ EL2/Kernel.lean
@@ -3,7 +3,7 @@ import EL2.Term
 namespace EL2
 
 
-partial def Term.inferTerm [Irreducible β] [Context Ctx (Term β)] (c: Term β) (ctx: Ctx) : Option (Term β × Ctx) := do
+partial def Term.infer [Irreducible β] [Context Ctx (Term β)] (c: Term β) (ctx: Ctx) : Option (Term β × Ctx) := do
   -- infer: turn everything to type then normalize
   match c with
     | .atom a =>
@@ -11,17 +11,17 @@ partial def Term.inferTerm [Irreducible β] [Context Ctx (Term β)] (c: Term β)
       pure (.atom p, ctx)
     | .var n =>
       let c : Term β ← Context.get? ctx n
-      c.inferTerm ctx
+      c.infer ctx
     | _ => sorry
 
-partial def Term.normalizeCode [Irreducible β] [Context Ctx (Term β)] (c: Term β) (ctx: Ctx): Option (Term β × Ctx) := do
+partial def Term.normalize [Irreducible β] [Context Ctx (Term β)] (c: Term β) (ctx: Ctx): Option (Term β × Ctx) := do
   -- normalize: just normalize
   match c with
     | .atom a =>
       pure (c, ctx) -- return itself
     | .var n =>
       let c: Term β ← Context.get? ctx n
-      c.normalizeCode ctx
+      c.normalize ctx
     | _ => sorry
 
 
