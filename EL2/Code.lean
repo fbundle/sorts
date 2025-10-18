379ccfcf898581d50e4db180b86ac740611788e9
khanh <khanh.nguyen.contact@gmail.com>
2025-10-19 00:19:27 +0800

    auto_push

--- EL2/Code.lean
+++ EL2/Code.lean
@@ -57,47 +57,6 @@ inductive Code (β: Type) where
   deriving Repr
 
 
-structure PrintCtx where
-  indentNum: Nat
-  indentSize: Nat
-
-def PrintCtx.next (ctx: PrintCtx): PrintCtx := {ctx with indentNum := ctx.indentNum+1}
-
-partial def PrintCtx.print [ToString β] (ctx: PrintCtx) (c: Code β): String :=
-  match c with
-    | .atom x => toString x
-    | .var n => n
-    | .list l =>
-      let indentStr := String.mk (List.replicate (ctx.indentNum * ctx.indentSize) ' ')
-      "\n"
-      ++
-      String.join (l.map (λ x => indentStr ++ (ctx.next.print x) ++ "\n"))
-    | .ann x => s!"({x.name}: {ctx.print x.type})"
-    | .bind_val x => s!"{x.name} := {ctx.print x.value}"
-    | .bind_typ x => s!"(type {x.name} {
-      String.join ((x.params.map (ctx.print ∘ (Code.ann ·))).intersperse " ")
-    })"
-    | .bind_mk x => s!"(type_mk {x.name} {
-      String.join ((x.params.map (ctx.print ∘ (Code.ann ·))).intersperse " ")
-    } -> " ++
-    match x.type.args with
-      | [] => x.type.cmd
-      | _ => s!"({x.type.cmd} {
-          String.join ((x.type.args.map ctx.print).intersperse " ")
-        }))"
-    | .app x =>
-      match x.args with
-        | [] => ctx.print x.cmd
-        | _ =>
-          s!"({ctx.print x.cmd} {
-            String.join ((x.args.map ctx.print).intersperse " ")
-          })"
-    | .lam x => s!"{
-      String.join ((x.params.map (ctx.print ∘ (Code.ann ·))).intersperse " ")
-    } => {ctx.print x.body}"
-
-instance [ToString β]: ToString (Code β) where
-  toString (c: Code β):= {indentNum := 0, indentSize := 2 :PrintCtx}.print c
 
 partial def Code.inferCode [Irreducible β] [Context Ctx (Code β)] (c: Code β) (ctx: Ctx) : Option (Code β × Ctx) := do
   -- infer: turn everything to type then normalize
---
+++ EL2/Print.lean
@@ -0,0 +1,46 @@
+import EL2.Code
+
+namespace EL2
+
+structure PrintCtx where
+  indentNum: Nat
+  indentSize: Nat
+
+def PrintCtx.next (ctx: PrintCtx): PrintCtx := {ctx with indentNum := ctx.indentNum+1}
+
+partial def PrintCtx.print [ToString β] (ctx: PrintCtx) (c: Code β): String :=
+  match c with
+    | .atom x => toString x
+    | .var n => n
+    | .list l =>
+      let indentStr := String.mk (List.replicate (ctx.indentNum * ctx.indentSize) ' ')
+      "\n"
+      ++
+      String.join (l.map (λ x => indentStr ++ (ctx.next.print x) ++ "\n"))
+    | .ann x => s!"({x.name}: {ctx.print x.type})"
+    | .bind_val x => s!"{x.name} := {ctx.print x.value}"
+    | .bind_typ x => s!"(type {x.name} {
+      String.join ((x.params.map (ctx.print ∘ (Code.ann ·))).intersperse " ")
+    })"
+    | .bind_mk x => s!"(type_mk {x.name} {
+      String.join ((x.params.map (ctx.print ∘ (Code.ann ·))).intersperse " ")
+    } -> " ++
+    match x.type.args with
+      | [] => x.type.cmd
+      | _ => s!"({x.type.cmd} {
+          String.join ((x.type.args.map ctx.print).intersperse " ")
+        }))"
+    | .app x =>
+      match x.args with
+        | [] => ctx.print x.cmd
+        | _ =>
+          s!"({ctx.print x.cmd} {
+            String.join ((x.args.map ctx.print).intersperse " ")
+          })"
+    | .lam x => s!"{
+      String.join ((x.params.map (ctx.print ∘ (Code.ann ·))).intersperse " ")
+    } => {ctx.print x.body}"
+
+instance [ToString β]: ToString (Code β) where
+  toString (c: Code β):= {indentNum := 0, indentSize := 2 :PrintCtx}.print c
+end EL2
