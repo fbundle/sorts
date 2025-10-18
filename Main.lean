08e4bbf4797b7bcffaee8c1372526bf42b421b83
khanh <khanh.nguyen.contact@gmail.com>
2025-10-18 22:29:17 +0800

    auto_push

--- Main.lean
+++ Main.lean
@@ -1,4 +1,3 @@
-import EL.EL
 import EL2.EL2
 import Std
 
@@ -47,19 +46,5 @@ end EL2_EXAMPLE
 
 
 
-def main (args : List String) : IO UInt32 := do
-  match args with
-  | [fileName] => do
-      let content ← IO.FS.readFile fileName
-      let tokens := EL.tokenize content
-      let result := Util.parseAll EL.parse tokens
-      if result.remaining.length ≠ 0 then
-        let remaining := String.join (result.remaining.intersperse " ")
-        IO.println s!"{repr result.items}\nerror at {remaining}"
-        return 1
-      else
-        IO.println s!"{repr result.items}"
-        return 0
-  | _ => do
-      IO.eprintln "Usage: el <file>"
-      return 1
+def main  : IO Unit := do
+  IO.println s!"{repr (EL2_EXAMPLE.el2 ())}"
