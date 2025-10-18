e914565379e946da6c1f1e308a4dff32deb706bf
khanh <khanh.nguyen.contact@gmail.com>
2025-10-19 00:18:32 +0800

    auto_push

--- EL2/Code.lean
+++ EL2/Code.lean
@@ -56,41 +56,48 @@ inductive Code (β: Type) where
   | lam: Lam (Code β) → Code β
   deriving Repr
 
-partial def Code.toString [ToString β] (c: Code β) (listIndent: Nat): String :=
-  let listIndentStr := String.mk (List.replicate (listIndent*2) ' ')
+
+structure PrintCtx where
+  indentNum: Nat
+  indentSize: Nat
+
+def PrintCtx.next (ctx: PrintCtx): PrintCtx := {ctx with indentNum := ctx.indentNum+1}
+
+partial def PrintCtx.print [ToString β] (ctx: PrintCtx) (c: Code β): String :=
   match c with
-    | .atom x => s!"(atom {x})"
+    | .atom x => toString x
     | .var n => n
-    | .list l => "[\n"
-    ++
-    String.join (l.map (λ x => listIndentStr ++ (x.toString (listIndent+1)) ++ "\n"))
-    ++ "]"
-    | .ann x => s!"({x.name}: {x.type.toString listIndent})"
-    | .bind_val x => s!"({x.name} := {x.value.toString listIndent})"
+    | .list l =>
+      let indentStr := String.mk (List.replicate (ctx.indentNum * ctx.indentSize) ' ')
+      "\n"
+      ++
+      String.join (l.map (λ x => indentStr ++ (ctx.next.print x) ++ "\n"))
+    | .ann x => s!"({x.name}: {ctx.print x.type})"
+    | .bind_val x => s!"{x.name} := {ctx.print x.value}"
     | .bind_typ x => s!"(type {x.name} {
-      String.join ((x.params.map ((Code.toString · listIndent) ∘ (Code.ann ·))).intersperse " ")
+      String.join ((x.params.map (ctx.print ∘ (Code.ann ·))).intersperse " ")
     })"
     | .bind_mk x => s!"(type_mk {x.name} {
-      String.join ((x.params.map ((Code.toString · listIndent) ∘ (Code.ann ·))).intersperse " ")
+      String.join ((x.params.map (ctx.print ∘ (Code.ann ·))).intersperse " ")
     } -> " ++
     match x.type.args with
       | [] => x.type.cmd
       | _ => s!"({x.type.cmd} {
-          String.join ((x.type.args.map (Code.toString · listIndent)).intersperse " ")
+          String.join ((x.type.args.map ctx.print).intersperse " ")
         }))"
     | .app x =>
       match x.args with
-        | [] => x.cmd.toString listIndent
+        | [] => ctx.print x.cmd
         | _ =>
-          s!"({x.cmd.toString listIndent} {
-            String.join ((x.args.map (Code.toString · listIndent)).intersperse " ")
+          s!"({ctx.print x.cmd} {
+            String.join ((x.args.map ctx.print).intersperse " ")
           })"
-    | .lam x => s!"({
-      String.join ((x.params.map ((Code.toString · listIndent) ∘ (Code.ann ·))).intersperse " ")
-    } => {x.body.toString listIndent})"
+    | .lam x => s!"{
+      String.join ((x.params.map (ctx.print ∘ (Code.ann ·))).intersperse " ")
+    } => {ctx.print x.body}"
 
 instance [ToString β]: ToString (Code β) where
-  toString (c: Code β):= Code.toString c 1
+  toString (c: Code β):= {indentNum := 0, indentSize := 2 :PrintCtx}.print c
 
 partial def Code.inferCode [Irreducible β] [Context Ctx (Code β)] (c: Code β) (ctx: Ctx) : Option (Code β × Ctx) := do
   -- infer: turn everything to type then normalize
