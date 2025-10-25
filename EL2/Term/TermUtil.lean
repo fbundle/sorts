d132309c7cdf0b666c0ca78e6a3696b809b4f492
khanh <khanh.nguyen.contact@gmail.com>
2025-10-26 01:58:47 +0800

    auto_push

--- EL2/Term/TermUtil.lean
+++ EL2/Term/TermUtil.lean
@@ -69,16 +69,79 @@ def T.optionMap? (t: T α) (f: α → Option β) : Option (T β) := do
         cases := cases,
       }
 
+
+def T.map (t: T α) (f: α → β) : T β :=
+  match t with
+    | T.inh x =>
+      let type := f x.type
+      let args := x.args.map f
+      T.inh {
+        type := type,
+        cons := x.cons,
+        args := args
+      }
+    | T.typ x =>
+      let value := f x.value
+      T.typ {
+        value := value,
+      }
+    | T.bnd x =>
+      let init := x.init.map (λ bind =>
+        let value := f bind.value
+        {
+          name := bind.name,
+          value := value,
+          : Bind β
+        }
+      )
+      let last := f x.last
+      T.bnd {
+        init := init,
+        last := last,
+      }
+    | T.lam x =>
+      let params := x.params.map (λ param =>
+        let type := f param.type
+        {
+          name := param.name,
+          type := type,
+          : Ann β
+        }
+      )
+      let body := f x.body
+      T.lam {
+        params := params,
+        body := body,
+      }
+    | T.app x =>
+      let cmd := f x.cmd
+      let args := x.args.map f
+      T.app {
+        cmd := cmd,
+        args := args,
+      }
+    | T.mat x =>
+      let cond := f x.cond
+      let cases := x.cases.map (λ case =>
+        let value := f case.value
+        {
+          patCmd := case.patCmd,
+          patArgs := case.patArgs,
+          value := value
+          : Case β
+        }
+      )
+      T.mat {
+        cond := cond,
+        cases := cases,
+      }
+
+
 def Term.map (term: Term) (f: Term → Term): Term :=
   match term with
     | .univ _ => term
     | .var _ => term
-    | .t x =>
-      match x.optionMap? (λ term => some (f term)) with
-        | none =>
-          dbg_trace "[UNREACHABLE]"
-          term -- unreachable
-        | some y => Term.t y
+    | .t x => Term.t (x.map f)
 
 def Term.optionMap? (term: Term) (f: Term → Option Term): Option Term := do
   match term with
