2c70cb6320c14f9ea78dec87b346898e6a9f33a0
khanh <khanh.nguyen.contact@gmail.com>
2025-10-28 02:10:20 +0800

    auto_push

--- WBT/WBTArr.lean
+++ WBT/WBTArr.lean
@@ -10,7 +10,17 @@ def WBTArr.empty : WBTArr α := {node? := none}
 partial def WBTArr.toArray (a: WBTArr α): Array α :=
   iterate a.node?
 
-def WBTArr.size (a: WBTArr α): Nat := weight a.node?
+def WBTArr.toList (a: WBTArr α): List α :=
+  a.toArray.toList
+
+def WBTArr.length (a: WBTArr α): Nat := weight a.node?
+def WBTArr.depth (a: WBTArr α): Nat := height a.node?
+
+instance [Repr α]: Repr (WBTArr α) where
+  reprPrec (a: WBTArr α) (_: Nat): Std.Format :=
+    s!"WBTArr(l={a.length}, d={a.depth})"
+
+
 
 partial def WBTArr.get? (a: WBTArr α) (i: Nat): Option α :=
   let rec loop (n?: Option (Node α)) (i: Nat): Option α :=
@@ -126,5 +136,24 @@ partial def WBTArr.mapM [Monad m] (a: WBTArr α) (f: α → m β): m (WBTArr β)
 
   let node? ← loop a.node?
   pure {node? := node? : WBTArr β}
+
+def WBTArr.push (a: WBTArr α) (x: α): WBTArr α :=
+  match a.insert? a.length x with
+    | none => sorry
+    | some a => a
+
+partial def WBTArr.fromList (xs: List α): WBTArr α :=
+  let rec loop (a: WBTArr α) (xs: List α): WBTArr α :=
+    match xs with
+    | [] => a
+    | x :: xs =>
+      loop (a.push x) xs
+
+  loop WBTArr.empty xs
+
+def WBTArr.fromArray (xs: Array α): WBTArr α :=
+  WBTArr.fromList xs.toList
+
+#eval WBTArr.fromArray (Array.replicate 99999 1)
 
 end WBT
