3da67af51ea533e28f3b5f440096afdf3eb77409
khanh <khanh.nguyen.contact@gmail.com>
2025-10-28 00:13:48 +0800

    auto_push

---
+++ WBT/WBTMap.lean
@@ -0,0 +1,20 @@
+import WBT.WBT
+
+namespace WBT
+
+
+
+structure WBTMap (α: Type u) (β: Type v) (cmp: α → α → Ordering) where
+  node? : Option (Node (α × β))
+
+-- the whole purpose of this self-balancing tree is to do this
+private structure A where
+  value : Nat
+  -- this only works for List, Array and don't work for Std.HashMap and Lean.RBTree
+  neighbours : WBTMap String A compare
+
+
+
+
+
+end WBT
