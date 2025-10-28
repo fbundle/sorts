9ee9ae132dd9d59c31df8a36e516219078dc30e5
khanh <khanh.nguyen.contact@gmail.com>
2025-10-28 22:43:29 +0800

    auto_push

--- EL2/Term/Reduce.lean
+++ EL2/Term/Reduce.lean
@@ -1,2 +1,16 @@
+import EL2.Term.Term
+import EL2.Term.TermUtil
+import EL2.Term.Util
+import EL2.Term.Print
+
 namespace EL2.Term.Infer
+
+
+structure InferedTerm where
+  term?: Option Term
+  type: Term -- type of term
+  level: Int -- level of term
+  deriving Repr
+
+
 end EL2.Term.Infer
