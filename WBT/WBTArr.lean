5f7d1dc1937b2faa25e2393a82c409f6aae1eb2c
Nguyen Ngoc Khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-10-28 10:45:03 +0800

    auto_push

--- WBT/WBTArr.lean
+++ WBT/WBTArr.lean
@@ -41,88 +41,65 @@ partial def WBTArr.get? (a: WBTArr α) (i: Nat): Option α :=
         none
 
 partial def WBTArr.set? (a: WBTArr α) (i: Nat) (x: α): Option (WBTArr α) := do
-  let rec loop (n?: Option (Node α)) (i: Nat): Option (Option (Node α)) := do
-    match n? with
-      | none => none
-      | some n =>
-        let (leftWeight, rightWeight) := (weight n.left?, weight n.right?)
-        if i < leftWeight then
-          let l1 ← loop n.left? i
-          let n1 := makeNode n.entry l1 n.right?
-          some (balance δ n1)
-        else if i = leftWeight then
-          let n1 := makeNode x n.left? n.right?
-          some n1
-        else if i < 1 + leftWeight + rightWeight then
-          let r1 ← loop n.right? (i - 1 - leftWeight)
-          let n1 := makeNode n.entry n.left? r1
-          some (balance δ n1)
-        else
-          none
+  match a.node? with
+    | none => none
+    | some n =>
+      let (leftWeight, rightWeight) := (weight n.left?, weight n.right?)
+      if i < leftWeight then
+        let l1 ← WBTArr.set? n.left? i x
+        let n1 := makeNode n.entry l1.node? n.right?
+        some (balance δ n1)
+      else if i = leftWeight then
+        let n1 := makeNode x n.left? n.right?
+        some (balance δ n1)
+      else if i < 1 + leftWeight + rightWeight then
+        let r1 ← WBTArr.set? n.right? (i - 1 - leftWeight) x
+        let n1 := makeNode n.entry n.left? r1.node?
+        some (balance δ n1)
+      else
+        none
 
-  let node? ← loop a.node? i
-  some {node? := node?}
+partial def WBTArr.insert? (a: WBTArr α) (i: Nat) (x: α): Option (WBTArr α) := do
+  match a.node? with
+    | none =>
+      if i ≠ 0 then none else some (makeNode x none none)
+    | some n =>
+      let (leftWeight, rightWeight) := (weight n.left?, weight n.right?)
+      if i ≤ leftWeight then
+        let l1 ← WBTArr.insert? n.left? i x
+        let n1 := makeNode n.entry l1.node? n.right?
+        some (balance δ n1)
+      else if i ≤ 1 + leftWeight + rightWeight then
+        let r1 ← WBTArr.insert? n.right? (i - 1 - leftWeight) x
+        let n1 := makeNode n.entry n.left? r1.node?
+        some (balance δ n1)
+      else
+        none
 
-partial def WBTArr.insert? (a: WBTArr α) (i: Nat) (x: α): WBTArr α :=
-  let rec loop (n?: Option (Node α)) (i: Nat): Option (Node α) :=
-    match n? with
-      | none =>
-        if i = 0 then
-          some (makeNode x none none)
-        else
-          none
-      | some n =>
-        let (leftWeight, rightWeight) := (weight n.left?, weight n.right?)
-        if i ≤ leftWeight then
-          let l1 := loop n.left? i
-          match loop n.left? i with
-            | none => none
-            | some l1 =>
-              let n1 := makeNode n.entry l1 n.right?
-              some (balance δ n1)
-        else if i ≤ 1 + leftWeight + rightWeight then
-          match loop n.right? (i - 1 - leftWeight) with
-            | none => none
-            | some r1 =>
-              let n1 := makeNode n.entry n.left? r1
-              some (balance δ n1)
-        else
-          none
+partial def WBTArr.delete? (a: WBTArr α) (i: Nat) : Option (WBTArr α) := do
+  match a.node? with
+    | none => none
+    | some n =>
+      let (leftWeight, rightWeight) := (weight n.left?, weight n.right?)
+      if i < leftWeight then
+        let l1 ← WBTArr.delete? n.left? i
+        let n1 := makeNode n.entry l1.node? n.right?
+        some (balance δ n1)
+      else if i = leftWeight then
+        match n.right? with
+          | none => n.left?
+          | some r =>
+            let x ← WBTArr.get? r 0
+            let r1 ← WBTArr.delete? r 0
+            let n1 := makeNode x n.left? r1.node?
+            some (balance δ n1)
+      else if i < 1 + leftWeight + rightWeight then
+        let r1 ← WBTArr.delete? n.right? (i - 1 - leftWeight)
+        let n1 := makeNode n.entry n.left? r1.node?
+        some (balance δ n1)
+      else
+        none
 
-  {node? := loop a.node? i}
-
-partial def WBTArr.delete? (a: WBTArr α) (i: Nat) : Option (WBTArr α) :=
-  let rec loop (n?: Option (Node α)) (i: Nat): Option (Node α) :=
-    match n? with
-      | none => none
-      | some n =>
-        let (leftWeight, rightWeight) := (weight n.left?, weight n.right?)
-        if i < leftWeight then
-          match loop n.left? i with
-            | none => none
-            | some l1 =>
-              let n1 := makeNode n.entry l1 n.right?
-              some (balance δ n1)
-        else if i = leftWeight then
-          if leftWeight = 0 then
-            n.right?
-          else
-            let newEntry? := {node? := n.left? : WBTArr α}.get? (leftWeight - 1)
-            match newEntry? with
-              | none => none
-              | some newEntry =>
-                let l1 := loop n.left? (leftWeight - 1)
-                let n1 := makeNode newEntry l1 n.right?
-                some (balance δ n1)
-        else if i < 1 + leftWeight + rightWeight then
-          match loop n.right? (i - 1 - leftWeight) with
-            | none => none
-            | some r1 =>
-              let n1 := makeNode n.entry n.left? r1
-              some (balance δ n1)
-        else
-          none
-  some {node? := loop a.node? i}
 
 partial def WBTArr.mapM [Monad m] (a: WBTArr α) (f: α → m β): m (WBTArr β) := do
   let rec loop (n?: Option (Node α)): m (Option (Node β)) := do
