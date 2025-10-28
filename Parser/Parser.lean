2b55e3b5a10fcbd5f39897c661b90e2628753adf
Nguyen Ngoc Khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-10-28 17:05:53 +0800

    auto_push

---
+++ Parser/Parser.lean
@@ -0,0 +1,15 @@
+namespace Parser
+-- parse ML-like language
+
+
+
+
+structure Ctx where
+  indent: Nat
+
+def Ctx.parse (source: )
+
+
+
+
+end Parser
---
+++ Parser/Tokenizer.lean
@@ -0,0 +1,10 @@
+namespace Parser
+
+def tokenize (source: String): List String :=
+  -- tokenize source
+  -- special tokens include ( ) \n <indent> : := =>
+
+
+  sorry
+
+end Parser
