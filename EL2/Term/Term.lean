be638d6dba05741dea070e86cea52517460d53e5
khanh <khanh.nguyen.contact@gmail.com>
2025-11-01 22:00:45 +0800

    auto_push

--- EL2/Term/Term.lean
+++ EL2/Term/Term.lean
@@ -12,7 +12,8 @@ inductive Term where
   | lam: (name: String) → (body: Term) → Term
   | pi:  (name: String) → (type: Term) → (body: Term) → Term
   | ind:
-    (name: String) →
+    (univ: Term) →                                           -- universe
+    (name: String) →                                         -- type name
     (params: List (String × Term)) →                        -- type params
     (cons: List (String × List (String × Term) × List Term)) →   -- constructors
     (body: Term) →
@@ -38,7 +39,14 @@ partial def curry (cmd: Term) (args: List Term): Term :=
     | arg :: rest =>
       curry (Term.app cmd arg) rest
 
+partial def chainBind (init: List (String × Term × Term)) (tail: Term): Term :=
+  match init with
+    | [] => tail
+    | (name, type, value) :: rest =>
+      Term.bnd name type value (chainBind rest tail)
+
 def scott
+  (univ: Term)
   (name: String)
   (params: List (String × Term))
   (cons: List (String × List (String × Term) × List Term))
@@ -46,7 +54,7 @@ def scott
     -- Scott encoding for inductive type
     -- ind works like bnd - it binds type name, and constructor name
     -- then give body
-    -- inductive T (p1: T1) (p2: T2) ... (pN: TN) where
+    -- inductive T (p1: T1) (p2: T2) ... (pN: TN): Univ where
     --    | cons1: (c11: V11) -> (c21: V21) ... -> (T x11 x21 ...)
     --    | ...
     --    | consM: (cM1: VM1) -> (cM2: VM2) ... -> (T x1M x2M ...)
@@ -54,17 +62,34 @@ def scott
     -- cons[1] := cons1 ((c11: V11) -> (c21: V21) ...) (x11 x21 ...)
 
 
-    let R := Term.var name
-
-    -- subTypeList (V11 -> V21 -> ...) -> ... -> (VM1 -> VM2 -> ...)
-    let subTypeList: List (String × Term) := cons.map (λ (consName, consParams, consBody) =>
-      let subType := chainPi consParams R
-      (consName, subType)
+  let R := Term.var "R"
+  let T := chainPi (
+    cons.map (λ (consName, consParams, _) =>
+      (consName, chainPi consParams R)
     )
-    -- params : (p1: T1) (p2: T2) ... (pN: TN)
-    let type := chainPi (params ++ subTypeList) R
+  ) R
 
-    sorry
+  let Tfunc := chainPi params T
+
+  let consNameList := cons.map (λ (consName, _, _) => consName)
+
+  let consNameTermTypeList := cons.map (λ (consName, consParams, consBody) =>
+    let x := chainLam (params.map (Prod.fst)) (chainLam consNameList (Term.var consName))
+    -- consTerm1 := λ c11 λ c21 ... (T x11 x21 ...)
+    let consTerm := chainLam (consParams.map (Prod.fst))
+      -- need anotated type since our core doesn't support untyped app
+      (curry (Term.ann x Tfunc) consBody)
+
+    -- type of consTerm
+    let consType := chainPi consParams T
+    (consName, consTerm, consType)
+  )
+
+  let consBind := chainBind consNameTermTypeList body
+  let typeBind := Term.bnd name Tfunc univ consBind
+
+  typeBind
+
 
 
 partial def Term.toExp (term: Term): Exp :=
@@ -76,17 +101,17 @@ partial def Term.toExp (term: Term): Exp :=
     | Term.ann term type => Exp.ann term.toExp type.toExp
     | Term.lam name body => Exp.lam name body.toExp
     | Term.pi name type body => Exp.pi name type.toExp body.toExp
-    | Term.ind name params cons body => (scott name params cons body).toExp
+    | Term.ind univ name params cons body => (scott univ name params cons body).toExp
 
 
 
 def test1: Term := -- Nat and Vec
   id
-  $ Term.ind "Nat" [] [
+  $ Term.ind (Term.typ 0) "Nat" [] [
     ("zero", [], []),
     ("succ", [("prev", Term.var "Nat")], []),
   ]
-  $ Term.ind "Vec0" [("n", Term.var "Nat"), ("T", Term.typ 0)] [
+  $ Term.ind (Term.typ 0) "Vec0" [("n", Term.var "Nat"), ("T", Term.typ 0)] [
     (
       "nil",
       [("T", Term.typ 0)],
@@ -107,6 +132,8 @@ def test1: Term := -- Nat and Vec
   $ Term.bnd "single_vec" (curry (Term.var "push") [Term.var "empty_vec"]) (curry (Term.var "Vec0") [Term.var "one", Term.var "Nat"])
   $ Term.typ 0
 
+#eval test1.toExp
 #eval typeCheck? test1.toExp (Term.typ 1).toExp
+
 
 end EL2.Term
