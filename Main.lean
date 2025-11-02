23d6f3486317822d0d2ef6aed98f07497b85852e
khanh <khanh.nguyen.contact@gmail.com>
2025-11-02 20:40:59 +0800

    auto_push

--- Main.lean
+++ Main.lean
@@ -1,7 +1,2 @@
-import EL2.Core.CoreV2
-open EL2.Core
-
 def main  : IO Unit := do
   IO.println "--------------------------------------"
-  IO.println s!"{test0.toExp.toString}"
-  IO.println s!"{typeCheck? test0 (Term.typ 0)}"
