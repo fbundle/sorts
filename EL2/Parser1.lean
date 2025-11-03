084fbadf0f3f69a6c2bdf99e5ba7ea3286612751
khanh <khanh.nguyen.contact@gmail.com>
2025-11-04 06:32:34 +0800

    auto_push

--- EL2/Parser1.lean
+++ EL2/Parser1.lean
@@ -12,12 +12,20 @@ mutual
 def parseExp: Parser Exp :=
   sorry
 
-def parseUniv
+def parseUniv: Parser Exp := λ xs => do
+  let (name, rest) ← Combinator.parseName xs
+  if "Type".isPrefixOf name then
+    let levelStr := name.stripPrefix "Type"
+    let level ← levelStr.toNat?
+    some (Exp.typ level, rest)
+  else
+    none
 
 
 
 
 end
+#eval parseUniv "Type123".toList
 
 
 end EL2.Parser1
