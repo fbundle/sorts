ded74e681e3430af134cbf623d3ebbd4ce0898b4
Nguyen Ngoc Khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-11-03 13:10:42 +0800

    auto_push

---
+++ EL2/Parser.lean
@@ -0,0 +1,70 @@
+namespace EL2.Parser.Internal
+
+def sortSplitTokens (splitTokens : List String) : List String :=
+  -- sort tokens so that if s2 is a prefix of s1, s1 should come first
+  let lessEqual (s1: String) (s2: String): Bool :=
+    if (s2.length < s1.length) && (s2.isPrefixOf s1) then true else
+    if (s1.length < s2.length) && (s1.isPrefixOf s2) then false else
+    s1 < s2
+
+  splitTokens.mergeSort lessEqual
+
+partial def stringIndexOf? (s: String) (substring: String): Option Nat :=
+  -- return the starting position of substring in s if exists
+  if substring.isEmpty then
+    some 0
+  else
+    let rec helper (s: String) (substring: String) (startIdx: Nat) : Option Nat :=
+      if startIdx + substring.length > s.length then
+        none
+      else if s.extract {byteIdx := startIdx} {byteIdx := startIdx + substring.length} = substring then
+        some startIdx
+      else
+        helper s substring (startIdx + 1)
+    helper s substring 0
+
+partial def splitPart (sortedSplitTokens : List String) (part : String) : List String :=
+  match sortedSplitTokens with
+    | [] => [part]
+    | s :: ss =>
+      match stringIndexOf? part s with
+        | some n =>
+          let before := part.take n
+          let after := part.drop (n + s.length)
+          let beforeParts := if before.isEmpty then [] else splitPart sortedSplitTokens before
+          let afterParts := if after.isEmpty then [] else splitPart sortedSplitTokens after
+          beforeParts ++ [s] ++ afterParts
+        | none => splitPart ss part
+
+def tokenize (sortedSplitTokens: List String) (s: String) : List String :=
+   -- remove comments
+  let lines := s.split (· = '\n')
+  let lines := lines.map (λ line =>
+    match line.splitOn "--" with
+      | head :: _ => head -- take everything before --
+      | _ => line
+  )
+  let s := String.join (lines.intersperse "\n")
+
+  -- basic tokenize
+  let parts := s.split (·.isWhitespace)
+
+  -- further tokenize by splitTokens
+  let output := parts.flatMap (splitPart sortedSplitTokens)
+
+  -- drop empty tokens
+  let output := output.filter (·.length > 0)
+
+  output
+
+def newTokenizer (splitTokens: List String): String → List String :=
+  tokenize (sortSplitTokens splitTokens)
+
+end EL2.Parser.Internal
+
+
+namespace EL2.Parser
+
+export EL2.Parser.Internal (newTokenizer)
+
+end EL2.Parser
